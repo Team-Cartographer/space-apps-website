fb9fd08cdb0f6a060fc938fb9cb7bd1caaee662f
Efe <suleymanefeclk2@gmail.com>
2023-10-08 20:30:47 -0700

    Updated the button

--- src/components/BetterButton.jsx
+++ src/components/BetterButton.jsx
@@ -1,10 +1,11 @@
 const BetterButton = () => {
   return (
     <a
-      href="/about-the-project"
+      href="https://docs.google.com/presentation/d/e/2PACX-1vThHnNV4OxnPd3OjOHvlWN_4U71MY5mHgLs6kkxX7UrffQfXY2gErtkajTe2V5pdZ01GW7Y1Zn0DGYM/pub?start=false&loop=false&delayms=30000"
+      target="_blank"
       className="absolute z-10 text-[#5a5757] right-0 hover:cursor-pointer mr-8 mt-8 text-2xl"
     >
-      About the Project
+      More Information
     </a>
   );
 };
