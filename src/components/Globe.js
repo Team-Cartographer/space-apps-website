83d9db890a263585c4983e6f55718c1115e7b4aa
Efe <suleymanefeclk2@gmail.com>
2023-10-08 19:37:24 -0700

    Added a little dot as representetive of the DSCVR satellite

--- src/components/Globe.js
+++ src/components/Globe.js
@@ -1,12 +1,25 @@
 import React, { useState, useEffect, useRef, useMemo } from "react";
 import Globe from "react-globe.gl";
 import * as d3 from "d3";
+import * as THREE from "three";
+
+const N = 300;
+const randomData = [
+  {
+    lat: 0,
+    lng: 0,
+    alt: 5,
+    radius: 10,
+    color: "red",
+  },
+];
 
 const World = ({ isPlaying, setAltitude, setIsReady }) => {
   const globeEl = useRef();
 
   const [countries, setCountries] = useState({ features: [] });
   const [hoverD, setHoverD] = useState();
+  const [data, setData] = useState(randomData);
 
   useEffect(() => {
     fetch("./country.json")
@@ -55,6 +68,19 @@ const World = ({ isPlaying, setAltitude, setIsReady }) => {
       }}
       onZoom={(e) => {
         setAltitude(e.altitude);
+      }}
+      customLayerData={data}
+      customThreeObject={(d) =>
+        new THREE.Mesh(
+          new THREE.SphereGeometry(d.radius),
+          new THREE.MeshLambertMaterial({ color: d.color })
+        )
+      }
+      customThreeObjectUpdate={(obj, d) => {
+        Object.assign(
+          obj.position,
+          globeEl.current.getCoords(d.lat, d.lng, d.alt)
+        );
       }}
     />
   );
